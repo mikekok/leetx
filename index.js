34fadcf4ff157492e30960bdddad4f554f8e97a6
Mike Kokkolios <mikekokkolios@gmail.com>
2017-06-19 11:45:35 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// Modules
+const request = require('request')
+const cheerio = require('cheerio')
+
+// 1337x.to base URL
+let leetxURL = 'http://1337x.to'
+
+module.exports = {
+
+  search: function(keyword, cb) {
+    let torrents = []
+    var reqURL = leetxURL + '/search/' + keyword + '/1/'
+    request(reqURL, function(err, res, body) {
+      var $ = cheerio.load(body)
+      $('table.table-list tr').each(function(index, el) {
+        var torrent = {}
+        torrent.name = $(this).find('td:nth-child(1) a:nth-child(2)').text()
+        torrent.seeds = $(this).find('td:nth-child(2)').text()
+        torrent.leechs = $(this).find('td:nth-child(3)').text()
+        torrent.url = leetURL + $(this).find('td:nth-child(1) a:nth-child(2)').attr('href')
+        if (torrent.name !== '') {
+          torrents.push(torrent)
+        }
+      })
+      return cb(null, torrents)
+    })
+  }
+
+}
